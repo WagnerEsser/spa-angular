2737eb247b80170effcdc5b0489723ad2f55f1cc
Wagner Esser <waesser@gmail.com>
2019-03-16 00:10:30 -0300

    add navbar bootstrap (responsível); alterado .sass para .scss; add logo no header (parte 2);

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  logo = '../../assets/pokemon-logo.png';
 
   constructor() { }
 
