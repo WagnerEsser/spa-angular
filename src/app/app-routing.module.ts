1e698cb973cc888d9d0c2223d6506e4c5cee4d65
Wagner Esser <waesser@gmail.com>
2019-03-15 22:01:39 -0300

    add rotas: home, page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: '404', component: PageNotFoundComponent },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
